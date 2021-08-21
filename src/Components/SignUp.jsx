831c59a2b610724b82d3bc6b97eb1fdb8a0de228
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2021-08-22 00:31:10 +0530

    signup

---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { auth } from "./Firebase";
+import { useHistory } from "react-router";
+const SignUp = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const history= useHistory()
+
+  const handleSubmit = async (event) => {
+    console.log(`
+      Email: ${email}
+      Password: ${password}
+     
+    `);
+
+    event.preventDefault();
+    try{
+        const result= await auth.createUserWithEmailAndPassword(email,password)
+        console.log(result);
+        history.pushState('/');
+    }
+    catch(err){
+        console.log(err);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Create Account</h1>
+
+      <label>
+        Email:
+        <input
+          name="email"
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required />
+      </label>
+<br/><br/>
+      <label>
+        Password:
+        <input
+          name="password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required />
+      </label>
+
+<br/>
+<br/>
+
+   
+
+      <button>Submit</button>
+    </form>
+  );
+}
+export default SignUp
