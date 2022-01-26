9904ae022d9b2be262f6ce305e81ac727a15b6a5
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2022-01-27 00:44:32 +0530

    Create Banner.jsx

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,38 @@
+// src/reusable/image-gallery.component.js
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import "../Styles/main.scss"
+import landing from "../Assets/images/landing.png"
+
+class ImageGallaryComponent extends React.Component {
+    render() {
+        return (
+            <>
+                <Carousel
+                  autoPlay
+        infiniteLoop
+        interval={3000}
+        showThumbs={false}
+        showStatus={false}
+        showArrows={false}
+        className="carousel-banner">
+                    <div   id="banner-container">
+                        <img src={landing} alt="LandingImage" />
+                        <div id="banner-text">My Classic Still 1</div>
+                    </div>
+                    <div   id="banner-container">
+                        <img src={landing}  alt="LandingImage" />
+                        <p className="legend">My Classic Still 2</p>
+                    </div>
+                    <div  id="banner-container">
+                        <img src={landing}  alt="LandingImage"/>
+                        <p className="legend">My Classic Still 3</p>
+                    </div>
+                </Carousel>
+            </>
+        )
+    };
+}
+
+export default ImageGallaryComponent;
