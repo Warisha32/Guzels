0adac39a3a64936c3c104a9e4a12cbc468a70c57
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2022-02-05 19:57:02 +0530

    Update Product.jsx

--- src/Components/Shopcomponents/Product.jsx
+++ src/Components/Shopcomponents/Product.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import "../../Styles/main.scss"
 import landing from "../../Assets/images/landing.png"
-
-const Product = () => {
+import { connect } from "react-redux";
+import {
+  loadCurrentItem,
+  addToCart,
+} from "../../Redux/Shopping/Actions";
+const Product = ({ product, addToCart, loadCurrentItem }) => {
     return(
 
 <div className="column">
 <div className="productImage">
-<img className="ProductPicture" src={landing} alt="img" />
+<img className="ProductPicture" src={product.image} alt="img" />
 </div>
 <div className="productText">
     <div className="Head">
-    <div className="chronicles_head">Creed Chronicles</div> <div className="priceTag">$15</div><br></br>
+    <div className="chronicles_head">{product.title}</div> <div className="priceTag">{product.price}</div><br></br>
     </div>
-    <div>Wanna be cool? Get this.<br></br> Trust us.</div>
+    <div>{product.description}</div>
 </div>
 <div className="button">
-<button className="Buy"><div className="buyText">Buy</div></button>
+<button className="Buy"onClick={() => addToCart(product.id)}><div className="buyText">Buy</div></button>
 </div>
 </div>
     )
  };
-  export default Product
+
+
+
+
+
+ const mapDispatchToProps = (dispatch) => {
+    return {
+      addToCart: (id) => dispatch(addToCart(id)),
+      loadCurrentItem: (item) => dispatch(loadCurrentItem(item)),
+    };
+  };
+
+  export default connect(null, mapDispatchToProps)(Product);
+
+ 
