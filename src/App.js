63dd8d9d610a96f68a19fc823d419c4c0865b358
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2022-02-08 02:33:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import React, {useState,useEffect} from 'react';
 import './App.css';
-// import 'firebase/auth';
-
+import { auth } from './Components/Firebase';
 import Home from './Pages/home';
 import Jwelery from './Pages/Jwelery';
 import Gifts from './Pages/Gifts';
 import Nav from './Components/NavBar';
 import Cart from './Pages/Cart/Cart';
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import SignUp from './Components/SignUp';
+import LogIn from './Components/LogIn';
+import Landing from './Pages/Cart/Landing';
+
 function App() {
   
+  const [user, setUser]=useState('');
+  useEffect( () => {
+    auth.onAuthStateChanged(user=>{
+      if(user) setUser(user)
+      else setUser(null)
+    })
+  },[])
   
-   
   return (
     <>
     < BrowserRouter >
-      <Nav/>
+    { 
+   user ?
+   <>
+   <Nav  user={user}/>
+   
+    
+   </> :
+   <>
+  <Landing/>
+      
+        </>
+   }
+    
+    
       <div>
         
       <Routes>
-        <Route path="/" element={<Home />} />
+      
+         <Route exact path="/Landing" element={<Landing />} />
         <Route path="/Home" element={<Home/>} />
         <Route path="/Jwelery" element={<Jwelery />} />
         <Route path="/Gifts" element={<Gifts />} />
-         <Route  path="/cart" element={<Cart/>} />
+         <Route  path="/cart" element={<Cart/>} />  
+         <Route  path="/LogIn" element={<LogIn/>} />
+         <Route  path="/SignUp" element={<SignUp/>} />
       </Routes>
      
 
